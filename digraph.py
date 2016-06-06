70f3ba1222a1a7e418fd0dd31cb35297da92cd42
Ashleigh <bunkas01@luther.edu>
2016-06-06 09:51:00 -0500

    Adding dummy implementations of DiGraph, Node, and TransformEdge classes.

---
+++ digraph.py
@@ -0,0 +1,25 @@
+__author__ = "Ashleigh"
+
+"""Placeholder Docstring"""
+
+
+class DiGraph:
+    def __init__(self, nodes= {}):
+        self.nodes = nodes
+
+    def get_all_nodes(self):
+        pass
+
+    def get_node(self):
+        pass
+
+    def add_node(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ transformedge.py
@@ -0,0 +1,35 @@
+__author__ = "Ashleigh"
+
+"""Placeholder Docstring"""
+
+
+class TransformEdge:
+    def __init__(self, parentNode, childNode, transformType="proportional"):
+        self.parentNode = parentNode
+        self.childNode = childNode
+        self. transformType = transformType
+
+    def get_parent_node(self):
+        pass
+
+    def set_parent_node(self):
+        pass
+
+    def get_child_node(self):
+        pass
+
+    def set_child_node(self):
+        pass
+
+    def get_transform_type(self):
+        pass
+
+    def set_transform_type(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
