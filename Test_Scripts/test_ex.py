96cb5995fe790e6b226a9c2305c2a9c63eddca6f
Ashleigh Bunkofske <bunkas01@luther.edu>
2016-07-07 17:01:37 -0500

    final fixes to test scripts completed, closes #8

--- Test_Scripts/test_ex.py
+++ Test_Scripts/test_ex.py
@@ -11,12 +11,12 @@ naming scheme for test scripts: < test_xx >, where xx is a number, e.g.
 # First, build the graph, adding all requisite vertices and edges, with
 # the appropriate relationships.
 aGraph = graph.Graph()
-aGraph.add_vertex("A", 10)
-aGraph.add_vertex("B", 10)
-aGraph.add_vertex("C", 10)
-aGraph.add_vertex("D", 10)
+aGraph.add_vertex("A", 100)
+aGraph.add_vertex("B", 100)
+aGraph.add_vertex("C", 100)
+aGraph.add_vertex("D", 100)
 aGraph.add_edge("A", "B", "aa_lin", [2, 2])
-aGraph.add_edge("A", "C", "pp_lin", [10, 15])
+aGraph.add_edge("A", "C", "ap_lin", [2, 5])
 aGraph.add_edge("B", "D", "aa_lin", [1])
 aGraph.add_edge("C", "D", "aa_lin", [1])
 aGraph.add_edge("D", "A", "aa_lin", [2])
@@ -26,7 +26,7 @@ aGraph.add_edge("D", "A", "aa_lin", [2])
 iDelta = {"A": 20, "B": 5}
 
 # Now, run the sim itself, for a specified number of cycles.
-output = deltacalc.gc_multicount_delta(aGraph, 10, iDelta)
+output = deltacalc.gc_multicount_delta(aGraph, 7, iDelta)
 
 # Finally, write the output to a spreadsheet to make using it easier.
 deltacalc.output_spreadsheet("test_ex_data", output)
