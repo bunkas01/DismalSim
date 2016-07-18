3b249f4682056d1e8fd00a479639d7d6e0129ccd
Linnea Peterson <peteli05@luther.edu>
2016-07-18 10:04:25 -0500

    Four more test scripts have been run: test_35 involves only NIR, not RIR, and includes feedback from IM and EX to FX; test_36 likewise involves only NIR but does not include the feedback; test_37 involves only RIR, not NIR, and includes the feedback; test_38 likewise involves only RIR but does not include the feedback.

---
+++ DismalSim/Test_Scripts/test_36.py
@@ -0,0 +1,58 @@
+from DismalSim.deltagraph import deltacalc
+from DismalSim.deltagraph import digraph
+
+"""Scripting the model from 1990-1999, with regressions 1980-1989.
+
+The script constructs a graph with only NIR and not RIR and runs it
+through the time period of 1990-1999; the regressions used to calculate
+edge relationships were performed on the time period 1980-1989. This
+graph does not include feedback from EX and IM to FX.
+"""
+
+aGraph = digraph.DiGraph()
+aGraph + digraph.Vertex("G", 1920.2, deltaInherent=-16.7)
+aGraph + digraph.Vertex("T", 1712.9, deltaInherent=3.02)
+aGraph + digraph.Vertex("YD", 4266.7)
+aGraph + digraph.Vertex("C", 3825.6, deltaInherent=7.20)
+aGraph + digraph.Vertex("IM", 629.7, deltaInherent=4.62)
+aGraph + digraph.Vertex("Y", 5979.6)
+aGraph + digraph.Vertex("EX", 551.9, deltaInherent=27.0)
+aGraph + digraph.Vertex("PL", 66.77, deltaInherent=-3.71)
+aGraph + digraph.Vertex("I", 993.5, deltaInherent=-79.0)
+aGraph + digraph.Vertex("FX", 71.41, deltaInherent=6.44)
+aGraph + digraph.Vertex("M2", 3223.58, deltaInherent=-66.0)
+aGraph + digraph.Vertex("NIR", 8.1, deltaInherent=-0.567)
+
+aGraph.add_edge("T", "G", "aa_lin", [1.28])
+aGraph.add_edge("Y", "T", "aa_lin", [0.302])
+aGraph.add_edge("Y", "YD", "aa_lin", [1])
+aGraph.add_edge("T", "YD", "aa_lin", [-1])
+aGraph.add_edge("YD", "C", "aa_lin", [0.943])
+aGraph.add_edge("NIR", "C", "aa_lin", [-3.41])
+aGraph.add_edge("Y", "IM", "aa_lin", [0.132])
+aGraph.add_edge("FX", "IM", "aa_lin", [-1.20])
+aGraph.add_edge("PL", "IM", "aa_lin", [-1.00])
+aGraph.add_edge("C", "Y", "aa_lin", [1])
+aGraph.add_edge("I", "Y", "aa_lin", [1])
+aGraph.add_edge("G", "Y", "aa_lin", [1])
+aGraph.add_edge("EX", "Y", "aa_lin", [1])
+aGraph.add_edge("IM", "Y", "aa_lin", [-1])
+aGraph.add_edge("FX", "EX", "aa_lin", [-7.98])
+aGraph.add_edge("PL", "EX", "aa_lin", [12.3])
+aGraph.add_edge("M2", "PL", "aa_lin", [0.0178])
+aGraph.add_edge("Y", "PL", "aa_lin", [0.00794])
+aGraph.add_edge("NIR", "I", "aa_lin", [-26.8])
+aGraph.add_edge("Y", "I", "aa_lin", [0.344])
+aGraph.add_edge("NIR", "FX", "aa_lin", [3.11])
+aGraph.add_edge("NIR", "M2", "aa_lin", [-8.01])
+aGraph.add_edge("Y", "M2", "aa_lin", [0.716])
+aGraph.add_edge("PL", "M2", "aa_lin", [-27.7])
+aGraph.add_edge("Y", "NIR", "aa_lin", [0.00181])
+aGraph.add_edge("PL", "NIR", "aa_lin", [0.951])
+
+initDelta = {"G": 114.4, "T": 50.4, "C": 134.6, "YD": 144, "IM": -6.2,
+             "Y": 194.4, "EX": 43, "PL": 5.5, "I": -49.2, "FX": 2.94,
+             "M2": 118.6, "NIR": -2.41}
+
+output = deltacalc.gc_multicount_delta(aGraph, 8, initDelta)
+deltacalc.output_spreadsheet("test_36_data", output)
